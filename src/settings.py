fcf6c07d254cb3aea114c5c2a754f3d16ca61c37
Juju-And <justyna.andrejczuk@gmail.com>
2020-11-07 18:09:36 +0100

    change allowed hosts add heroku

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cars-rate.herokuapp.com']
 
 # Application definition
 
