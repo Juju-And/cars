4f5fe9d3a3b0bda8902ff23398cf98e94c8e1058
Juju-And <justyna.andrejczuk@gmail.com>
2020-11-07 17:28:43 +0100

    fix docker + heroku deployment file

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,11 @@
 FROM python:3.8-alpine
 
-ENV PATH="/scripts:${PATH}"
-
 COPY ./requirements.txt /requirements.txt
 RUN apk add --update --no-cache --virtual .tmp gcc libc-dev linux-headers
 RUN pip install -r requirements.txt
-RUN apk del .tmp
 
-RUN mkdir /cars
-COPY ./ /cars
-WORKDIR /cars
-COPY ./scripts /scripts
+RUN mkdir /app
+COPY ./ /app
+WORKDIR /app
 
-RUN chmod +x /scripts/*
-
-# copy project
-COPY . .
-
-# add and run as non-root user
-RUN adduser -D myuser
-USER myuser
-
-# run gunicorn
-CMD gunicorn carmanager.wsgi:application --bind 0.0.0.0:$PORT
-
-CMD ["entrypoint.sh"]
-
-CMD ["python", "manage.py", "runserver"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- src/settings.py
+++ src/settings.py
@@ -20,16 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
-
-if ALLOWED_HOSTS_ENV:
-    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
